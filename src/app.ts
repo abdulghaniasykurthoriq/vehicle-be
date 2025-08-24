9f3c850659644e6ae3ed144944a66ff27a92c57c
Abdul Ghani Asykur Thoriq <abdulghaniasykurthoriq>
2025-08-24 23:30:24 +0700

    fix: add api fe yg di publish

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@ export function createApp(deps?: Partial<ReturnType<typeof buildServices>>) {
   app.set("trust proxy", true);
   app.use(
     cors({
-      origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+      origin: ["http://localhost:5173", "http://127.0.0.1:5173", "https://103.183.75.108"],
       credentials: true,
       methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
       allowedHeaders: ["Content-Type", "Authorization"],
