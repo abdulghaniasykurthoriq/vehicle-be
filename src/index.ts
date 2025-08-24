2eb8ecf25ee9371cae64f4f4a460a459e66459aa
Abdul Ghani Asykur Thoriq <abdulghaniasykurthoriq>
2025-08-24 09:49:29 +0700

    fix: api public

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,14 @@ import "dotenv/config";
 import { createApp } from "./app";
 import { config } from "./config";
 
+
 const app = createApp();
-app.listen(config.port, () => {
-  console.log(`🚗 Vehicle API listening on http://localhost:${config.port}`);
+app.listen(config.port, "0.0.0.0", () => {
+  console.log(`🚗 Vehicle API listening on 0.0.0.0:${config.port}`);
   console.log(`📚 Swagger UI at http://localhost:${config.port}/docs`);
 });
+// const app = createApp();
+// app.listen(config.port, () => {
+//   console.log(`🚗 Vehicle API listening on http://localhost:${config.port}`);
+//   console.log(`📚 Swagger UI at http://localhost:${config.port}/docs`);
+// });
